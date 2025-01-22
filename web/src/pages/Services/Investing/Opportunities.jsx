3d4c91ac969e91102f57e50633ea6d419dee8e7f
Isaiah Vickers <isaiah.vickers@outlook.com>
2025-01-22 17:32:17 -0500

    updates to platform services page

--- web/src/pages/Services/Investing/Opportunities.jsx
+++ web/src/pages/Services/Investing/Opportunities.jsx
@@ -10,7 +10,7 @@ const Opportunities = () => {
           "url('https://plus.unsplash.com/premium_photo-1674646022401-6b8bcae30506?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"
       }}
     >
-      {/* Dark Green Overlay */}
+      {/* Overlay */}
       <div className="absolute inset-0 bg-accent-primary bg-opacity-80"></div>
 
       {/* Content */}
