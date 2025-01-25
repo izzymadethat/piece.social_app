4d24739f7e4ae19e0d7a4cdee944db0ce7676805
Isaiah Vickers <isaiah.vickers@outlook.com>
2025-01-25 10:06:08 -0500

    update(about): update about page layout
    - Added a prototype 'How can I help' section
    - Updated Goals section to have grandiose goal

--- web/src/pages/About/Goals.jsx
+++ web/src/pages/About/Goals.jsx
@@ -3,8 +3,12 @@ import Timeline from "./Timeline";
 const Goals = () => {
   return (
     <section className="flex flex-col items-center justify-center p-24">
-      <h2 className="my-8 text-5xl">Our 2025 Goals</h2>
-      <Timeline />
+      <h2 className="my-8 text-5xl">Our Mission</h2>
+      {/* <Timeline /> */}
+      <p className="text-lg text-center">
+        It is our goal to help <span className="font-extrabold">1 MILLION</span>{" "}
+        people in need by 2030
+      </p>
     </section>
   );
 };
