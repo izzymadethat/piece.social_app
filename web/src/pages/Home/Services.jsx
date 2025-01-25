c73c2de20193c2e91747491f72a71e82a65e95b0
Isaiah Vickers <isaiah.vickers@outlook.com>
2025-01-25 09:24:26 -0500

    update(homepage): update services section
    - Piece.social Services may look better between the
        2 solution boxes -Fixed

--- web/src/pages/Home/Services.jsx
+++ web/src/pages/Home/Services.jsx
@@ -3,17 +3,18 @@ import { targets } from "../../utils/targets";
 
 const Services = () => {
   return (
-    <section className="grid grid-cols-1 lg:grid-cols-7">
-      <div className="p-8 lg:col-span-3 flex-center-center lg:p-0">
+    <section className="grid grid-cols-1 px-4 pb-16 lg:grid-cols-3">
+      <div className="p-8 flex-center-center lg:p-0 lg:order-2">
         <h3 className="text-2xl">
           Piece.Social <br />{" "}
           <span className="text-4xl text-accent-secondary">Services</span>
         </h3>
       </div>
-      <div className="flex flex-col lg:col-span-4 lg:flex-row">
-        {targets.map((info, i) => (
-          <SectionCard info={info} key={i} />
-        ))}
+      <div className="flex flex-col lg:flex-row lg:order-1">
+        <SectionCard info={targets[0]} />
+      </div>
+      <div className="flex flex-col lg:flex-row lg:order-3">
+        <SectionCard info={targets[1]} />
       </div>
     </section>
   );
