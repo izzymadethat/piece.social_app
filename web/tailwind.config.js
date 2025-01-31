61ed28ce9647d31f806b3b22ca7b0605b42255c9
Isaiah Vickers <140284664+izzymadethat@users.noreply.github.com>
2025-01-31 09:00:32 -0500

    Refactor/monorepo-setup (#9)
    * setup root package.json
    
    - removed devDeps such as biome from web directory to add to root
    
    * style: format code with new monorepo settings

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,29 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./node_modules/rizzui/dist/*.{js,ts,jsx,tsx}",
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        heading: ["EB Garamond", "serif"],
-        body: ["IBM Plex Sans", "sans-serif"]
-      },
-      screens: {
-        lg: { min: "925px" }
-      },
-      colors: {
-        primary: "#0D1216",
-        secondary: "#AACBBC",
-        "accent-primary": "#EEE3A9",
-        "accent-secondary": "#1A734A",
+	content: [
+		"./node_modules/rizzui/dist/*.{js,ts,jsx,tsx}",
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				heading: ["EB Garamond", "serif"],
+				body: ["IBM Plex Sans", "sans-serif"],
+			},
+			screens: {
+				lg: { min: "925px" },
+			},
+			colors: {
+				primary: "#0D1216",
+				secondary: "#AACBBC",
+				"accent-primary": "#EEE3A9",
+				"accent-secondary": "#1A734A",
 
-        foreground: "#0B2016",
-        background: "#F3FAF3"
-      }
-    }
-  },
-  plugins: []
+				foreground: "#0B2016",
+				background: "#F3FAF3",
+			},
+		},
+	},
+	plugins: [],
 };
